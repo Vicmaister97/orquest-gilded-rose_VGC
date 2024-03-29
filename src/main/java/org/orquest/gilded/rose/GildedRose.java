9caaa0a05632674b3c9181e1a4929cf4c8f6e7e9
VK <victorgarciacarrera@gmail.com>
2024-03-29 15:31:37 +0100

    Fixed indendation error.

--- src/main/java/org/orquest/gilded/rose/GildedRose.java
+++ src/main/java/org/orquest/gilded/rose/GildedRose.java
@@ -74,9 +74,9 @@ class GildedRose {
                                     item.quality = item.quality - 1;
                                 }
                             }
-                        } else {
-                            item.quality = 0;
                         }
+                    } else {
+                        item.quality = 0;
                     }
                 } else {
                     if (item.quality < 50) {
